109bf82e3eebd281ea4f38e35b23e680c8dc3b4d
Thiago Padovani <reycisco@MBP-Thiago.home>
2019-04-29 10:46:25 +0100

    First commit

---
+++ project3-film-festival/scripts.js
@@ -0,0 +1,26 @@
+//scroll function
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+//menu mobile function
+function mobile_menu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
